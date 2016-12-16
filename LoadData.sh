df1c9a6f9aca54f0349e94bb7f282b84e3532789
bitnami <bitnami@ubuntu.defaultdomain>
2016-12-16 18:29:36 +0000

    LoadData.sh modified

--- LoadData.sh
+++ LoadData.sh
@@ -2,14 +2,20 @@
 
 clear
 
-mysql -u root --password=admin < ./CreateDatabase.sql
-mysql -u root --password=admin EMPLOYEES < ./SQLScripts/DepartmentTable.sql
-mysql -u root --password=admin EMPLOYEES < ./SQLScripts/DepartmentUpdate.sql
-mysql -u root --password=admin EMPLOYEES < ./SQLScripts/EmployeeTable.sql
-mysql -u root --password=admin EMPLOYEES < ./SQLScripts/EmployeeUpdate.sql
-mysql -u root --password=admin EMPLOYEES < ./SQLScripts/ProjectTable.sql
-mysql -u root --password=admin EMPLOYEES < ./SQLScripts/ProjectUpdate.sql
-mysql -u root --password=admin EMPLOYEES < ./SQLScripts/WorksOnTable.sql
-mysql -u root --password=admin EMPLOYEES < ./SQLScripts/WorksOnUpdate.sql
-mysql -u root --password=admin EMPLOYEES < ./SQLScripts/CreateUser.sql
-mysql -u root --password=admin EMPLOYEES < ./SQLScripts/InsertData.sql
+
+echo -n "Enter root user in mysql --> "
+read root_name
+echo -n "Enter root password in mysql -->"
+read root_pass
+
+mysql -u $root_name --password=$root_pass < ./SQLScripts/CreateDatabase.sql
+mysql -u $root_name --password=$root_pass EMPLOYEES < ./SQLScripts/DepartmentTable.sql
+mysql -u $root_name --password=$root_pass EMPLOYEES < ./SQLScripts/DepartmentUpdate.sql
+mysql -u $root_name --password=$root_pass EMPLOYEES < ./SQLScripts/EmployeeTable.sql
+mysql -u $root_name --password=$root_pass EMPLOYEES < ./SQLScripts/EmployeeUpdate.sql
+mysql -u $root_name --password=$root_pass EMPLOYEES < ./SQLScripts/ProjectTable.sql
+mysql -u $root_name --password=$root_pass EMPLOYEES < ./SQLScripts/ProjectUpdate.sql
+mysql -u $root_name --password=$root_pass EMPLOYEES < ./SQLScripts/WorksOnTable.sql
+mysql -u $root_name --password=$root_pass EMPLOYEES < ./SQLScripts/WorksOnUpdate.sql
+mysql -u $root_name --password=$root_pass EMPLOYEES < ./SQLScripts/CreateUser.sql
+mysql -u $root_name --password=$root_pass EMPLOYEES < ./SQLScripts/InsertData.sql
